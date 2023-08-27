bb583a1b05ba542c1228c078b2ac512f193792f4
Huey-dev <matthewokadinya@gmail.com>
2023-08-27 12:49:28 +0100

    Fixed Rocketpupup to display images

--- src/components/RocketPopup.jsx
+++ src/components/RocketPopup.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const RocketPopup = ({ rocket, onClosePopup }) => {
   return (
     <div className="rocket-popup">
-      {/* Display detailed rocket information */}
+      <button className="close-button" onClick={onClosePopup}>
+        Close
+      </button>
+      <h2>{rocket.name}</h2>
+      <img src={rocket.image} alt={rocket.name} />
+      {/* Display other detailed rocket information */}
     </div>
   );
 };
