15949e8c3f57f3a427797cfda9e7c9267a87728d
Huey-dev <matthewokadinya@gmail.com>
2023-08-27 12:19:07 +0100

    utils deleted, appjs ued as entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Header from './components/Header';
+import SearchBar from './components/SearchBar';
+import RocketCard from './components/RocketCard';
+import RocketPopup from './components/RocketPopup';
 
-const App = () => {
+function App() {
+  const [rockets, setRockets] = useState([]); // State to store fetched rockets
+  const [selectedRocket, setSelectedRocket] = useState(null); // State to manage selected rocket for popup
+
+  // Fetch rockets from the SpaceX API and set them in the state
+  const fetchRockets = async () => {
+    try {
+      const response = await fetch("https://api.spacexdata.com/v4/rockets");
+      const data = await response.json();
+      setRockets(data);
+    } catch (error) {
+      console.error("Error fetching rockets:", error);
+    }
+  };
+
+  // Handler for rocket card click, opens the popup
+  const handleRocketClick = (rocket) => {
+    setSelectedRocket(rocket);
+  };
+
+  // Handler to close the rocket popup
+  const handleClosePopup = () => {
+    setSelectedRocket(null);
+  };
+
   return (
-    <>
-    <div className='flex justify-center items-center h-screen bg-black'>
-      <h1 className="text-3xl font-bold underline text-white rounded-lg p-4">
-          Hello world!
-      </h1>
+    <div className="app">
+      <Header />
+      <SearchBar />
+      <section className="rocket-grid">
+        {rockets.map((rocket) => (
+          <RocketCard
+            key={rocket.id}
+            rocket={rocket}
+            onRocketClick={handleRocketClick}
+          />
+        ))}
+      </section>
+      {selectedRocket && (
+        <RocketPopup rocket={selectedRocket} onClosePopup={handleClosePopup} />
+      )}
     </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
