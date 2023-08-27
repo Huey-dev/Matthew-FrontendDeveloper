b59904ce14559164cbc6eb8c3ae4b5c6ad00e1d6
Huey-dev <matthewokadinya@gmail.com>
2023-08-27 22:00:20 +0100

    searchBar and header styled!

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,23 +7,32 @@ function SearchBar({ onFilterChange, onApplyFilters }) {
   });
 
   return (
-    <div>
-      <select
-        value={filters.filterType}
-        onChange={(e) => setFilters({ ...filters, filterType: e.target.value })}
-      >
-        <option value="">Select Filter Type</option>
-        <option value="status">Status</option>
-        <option value="original_launch">Original Launch</option>
-        <option value="type">Type</option>
-      </select>
-      <input
-        type="text"
-        placeholder="Filter Value"
-        value={filters.filterValue}
-        onChange={(e) => setFilters({ ...filters, filterValue: e.target.value })}
-      />
-      <button onClick={() => onApplyFilters(filters)}>Search</button>
+    <div className="flex flex-col items-center my-8">
+      <div className="flex space-x-2">
+        <select
+          className="p-2 border rounded bg-gray-100"
+          value={filters.filterType}
+          onChange={(e) => setFilters({ ...filters, filterType: e.target.value })}
+        >
+          <option value="">Select Filter Type</option>
+          <option value="status">Status</option>
+          <option value="original_launch">Original Launch</option>
+          <option value="type">Type</option>
+        </select>
+        <input
+          className="p-2 border rounded"
+          type="text"
+          placeholder="Filter Value"
+          value={filters.filterValue}
+          onChange={(e) => setFilters({ ...filters, filterValue: e.target.value })}
+        />
+        <button
+          className="p-2 bg-gray-900 text-white rounded"
+          onClick={() => onApplyFilters(filters)}
+        >
+          Search
+        </button>
+      </div>
     </div>
   );
 }
